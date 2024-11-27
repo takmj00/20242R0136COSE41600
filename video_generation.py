201ddce0a5c9032794726116f940ed3816f35484
takmj00 <takmj00@gmail.com>
2024-11-27 17:53:37 +0900

    modify: to use draw_bbox function

--- video_generation.py
+++ video_generation.py
@@ -3,23 +3,17 @@ import cv2
 import numpy as np
 import os
 
-def video_generation(pcd_dir, output_video, view_param_file='view_params.json'):
+from draw_bbox import draw_bbox
 
+def video_generation(pcd_dir, output_video):
+    
+    view_param_file='view_params.json'
+    point_size = 1.0
     frame_width = 1920
     frame_height = 1054
-    fps = 30
+    fps = 20
 
     pcd_files = sorted([f for f in os.listdir(pcd_dir) if f.endswith('.pcd')])
-
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter(output_video, fourcc, fps, (frame_width, frame_height))
-
-
-    vis = o3d.visualization.Visualizer()
-    vis.create_window(width=frame_width, height=frame_height)
-    vis.get_render_option().point_size = 1
-
-    view_ctl = vis.get_view_control()
 
     custom_view_parameters = None
 
@@ -27,6 +21,11 @@ def video_generation(pcd_dir, output_video, view_param_file='view_params.json'):
         custom_view_parameters = o3d.io.read_pinhole_camera_parameters(view_param_file)
         print("Loaded saved view parameters.")
     else:
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(width=frame_width, height=frame_height)
+        vis.get_render_option().point_size = point_size
+        view_ctl = vis.get_view_control()
+
         pcd = o3d.io.read_point_cloud(os.path.join(pcd_dir, pcd_files[0]))
         vis.add_geometry(pcd)
         
@@ -38,19 +37,26 @@ def video_generation(pcd_dir, output_video, view_param_file='view_params.json'):
         print(f"View parameters saved to '{view_param_file}' file.")
         
         vis.clear_geometries()
+        vis.destroy_window()
 
-    vis.destroy_window()
+
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter(output_video, fourcc, fps, (frame_width, frame_height))
+
+    vis = o3d.visualization.Visualizer()
     vis.create_window(width=frame_width, height=frame_height, visible=False)
-    opt = vis.get_render_option()
-    vis.get_render_option().point_size = 1
-
+    vis.get_render_option().point_size = point_size
     view_ctl = vis.get_view_control()
 
     for pcd_file in pcd_files:
         vis.clear_geometries()
-        pcd = o3d.io.read_point_cloud(os.path.join(pcd_dir, pcd_file))
+        file_path = os.path.join(pcd_dir, pcd_file)
+        pcd, bounding_boxes = draw_bbox(file_path)
         vis.add_geometry(pcd)
-
+        for bbox in bounding_boxes:
+            vis.add_geometry(bbox)
+        
         view_ctl.convert_from_pinhole_camera_parameters(custom_view_parameters, allow_arbitrary=True)
 
         vis.poll_events()
@@ -66,4 +72,4 @@ def video_generation(pcd_dir, output_video, view_param_file='view_params.json'):
     vis.destroy_window()
 
 if __name__ == "__main__":
-    video_generation("data/01_straight_walk/pcd", "output_video.mp4")
+    video_generation("data/01_straight_walk/pcd", "01_straight_walk.mp4")
